f71d0dde4e0ca981c8253c3af645950ff924ec63
banti-kumar9128 <vk9128697037@gmail.com>
2025-12-27 21:52:13 +0530

    Remove unnecessary dotenv imports and fix API requests

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import dotenv from "dotenv";
-dotenv.config();
 
 const SignUp = () => {
   const navigate = useNavigate();
